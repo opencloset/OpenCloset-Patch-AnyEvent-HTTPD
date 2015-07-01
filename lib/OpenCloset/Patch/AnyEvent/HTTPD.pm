b741490b59b242760c830bfc685436160d8afea3
Keedi Kim <keedi.k@gmail.com>
2015-07-01 17:42:41 +0900

    Ready to dzil

---
+++ lib/OpenCloset/Patch/AnyEvent/HTTPD.pm
@@ -0,0 +1,24 @@
+package OpenCloset::Patch::AnyEvent::HTTPD;
+# ABSTRACT: OpenCloset AnyEvent::HTTPD Patch
+
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+    use OpenCloset::Patch::AnyEvent::HTTPD;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
